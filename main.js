38686371aa73413e7a7575323baead5fd1b3728f
dibmens <dibmens@gmail.com>
2025-07-27 23:44:45 +0300

    Add delete node method

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 import Tree from "./search-tree.js"
 
-let sortedArr = [1, 2, 3, 4, 5, 6, 8];
+let dataArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
-let test = new Tree(sortedArr);
+let test = new Tree(dataArray);
 // console.log(test.root);
 // console.log(test.find(3))
 
-test.insert(14)
-test.insert(11)
-test.insert(9)
-test.insert(10)
-test.insert(6)
-test.insert(7)
+// test.insert(14)
+// test.insert(11)
+// test.insert(9)
+// test.insert(10)
+// test.insert(6)
+// test.delete(8)
+// test.delete(1)
+// test.delete(67)
+// test.delete(4)
+// test.delete(6345)
+// test.delete(324)
+// test.delete(3)
+
+test.delete(4)
+test.delete(5)
+// test.delete(67)
+// test.delete(324)
+// test.delete(23)
+// test.delete(9)
+// test.delete(8)
 test.prettyPrint(test.root)
--- search-tree.js
+++ search-tree.js
@@ -1,6 +1,39 @@
 export default class Tree {
-  constructor(dataArray) {
-    this.root = this.buildTree(dataArray)
+  constructor(inputArray) {
+    this.root = this.buildTree(this.sortArray(inputArray));
+  }
+
+  sortArray(array) {
+    function merge(left, right) {
+      let sortedArr = [];
+
+      while (left.length && right.length) {
+        if (left[0] < right[0]) {
+          sortedArr.push(left.shift());
+        } else {
+          sortedArr.push(right.shift());
+        }
+      }
+      return [...sortedArr, ...left, ...right];
+    }
+
+    function mergeSort(array) {
+      const half = array.length / 2;
+
+      if (array.length < 2) {
+        return array;
+      }
+      const left = array.splice(0, half);
+
+      return merge(mergeSort(left), mergeSort(array));
+    }
+
+    function removeDuplicates(array) {
+      return Array.from(new Set(array));
+    }
+
+    let cleanedArray = removeDuplicates(array);
+    return mergeSort(cleanedArray);
   }
 
   Node(data) {
@@ -28,59 +61,100 @@ export default class Tree {
     }
   }
 
-  find(value){
-    let node = this.root
-    while(node){
-      if(value > node.data){
-        node = node.right
-      } else if (value < node.data){
-        node = node.left
+  find(value) {
+    let node = this.root;
+    while (node) {
+      if (value > node.data) {
+        node = node.right;
+      } else if (value < node.data) {
+        node = node.left;
       } else {
-        return node
+        return node;
       }
     }
-    return node
+    return node;
   }
 
-  prettyPrint(node, prefix = '', isLeft = true){
+  prettyPrint(node, prefix = "", isLeft = true) {
     if (node === null) {
       return;
     }
     if (node.right !== null) {
-      this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
     }
-    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
     if (node.left !== null) {
-      this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-    } 
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
   }
 
-  insert(value){
-    let node = this.root
-    
-    while(node){
-      if(value < node.data){
-        if(!node.left){
-          node.left = this.Node(value)
-          return
+  insert(value) {
+    let node = this.root;
+
+    while (node) {
+      if (value < node.data) {
+        if (!node.left) {
+          node.left = this.Node(value);
+          return;
         } else {
-          node = node.left
+          node = node.left;
         }
-      } else if (value > node.data){
-        if(!node.right){
-          node.right = this.Node(value)
-          return
+      } else if (value > node.data) {
+        if (!node.right) {
+          node.right = this.Node(value);
+          return;
         } else {
-          node = node.right
+          node = node.right;
         }
-      } else if (value == node.data){
-        return console.log(`Value ${value} already exists.`)
+      } else if (value == node.data) {
+        return console.log(`Value ${value} already exists.`);
       }
     }
-    
   }
 
+  delete(value) {
+    let node = this.root;
+    let parentNode = node;
+    let direction;
 
+    while (node) {
+      if (value < node.data) {
+        direction = "left";
+      } else if (value > node.data) {
+        direction = "right";
+      } else if (value == node.data) {
+        if (!node.left && !node.right) {
+          parentNode[direction] = null;
+        } else if (node.left && !node.right) {
+          parentNode[direction] = node.left;
+        } else if (!node.left && node.right) {
+          parentNode[direction] = node.right;
+        } else if (node.left && node.right) {
+          let replacementNode = node.right;
+          let replacementParent = node.right;
 
-
+          while (replacementNode.left) {
+            replacementParent = replacementNode;
+            replacementNode = replacementNode.left;
+          }
+          node.data = replacementNode.data;
+          replacementParent.left = null;
+          if (replacementNode.right) {
+            replacementParent.left = replacementNode.right;
+          }
+          //bad solution, rewrite to an if case that properly 
+          // removes childless right child of deleted node
+          if (node.data == node.right.data) {
+            node.right = null;
+          }
+        }
+      }
+      parentNode = node;
+      node = node[direction];
+    }
+  }
 }
