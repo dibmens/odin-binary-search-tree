e8d811d7a793ee88dd2ff801ef21821a5dd09365
dibmens <dibmens@gmail.com>
2025-08-01 23:00:03 +0300

    Add required tests

--- main.js
+++ main.js
@@ -1,39 +1,60 @@
 import Tree from "./search-tree.js"
 
-let dataArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+function generateRandomNumArray(arraySize,maxNumValue){
+    let arr = []
+    for(let i = arraySize; i>0; i--){
+        let num = Math.floor(Math.random()*maxNumValue+1)
+        arr.push(num)
+    }
+    return arr
+}
 
+let dataArray = generateRandomNumArray(14,99)
 let test = new Tree(dataArray);
-// console.log(test.root);
-// console.log(test.find(3))
 
-// test.insert(14)
-// test.insert(11)
-// test.insert(10)
-// test.insert(6)
-// test.delete(8)
-// test.delete(1)
-// test.delete(67)
-// test.delete(4)
-// test.insert(11)
-// test.insert(15)
-// test.insert(13)
-test.insert(2)
-// test.insert(14)
-// test.insert(16)
-// test.insert(12)
-
-
-// test.delete(4)
-// test.delete(5)
-// test.delete(67)
-// test.delete(324)
-// test.delete(23)
-// test.delete(9)
-// test.delete(8)
-// test.prettyPrint(test.root)
-test.rebalance()
 test.prettyPrint(test.root)
-// console.log(`Tree height is ${test.height(8)}`)
-// console.log(test.depth(8))
+console.log(`Current tree is balanced:`,test.isBalanced())
 
-console.log(test.isBalanced())
+let testArray1 = new Array()
+test.levelOrderForEach(x => testArray1.push(x.data))
+console.log(`Level order method: `,testArray1)
+
+let testArray2 = new Array()
+test.preOrderForEach(x => testArray2.push(x.data))
+console.log(`Pre-order method: `,testArray2)
+
+let testArray3 = new Array()
+test.postOrderForEach(x => testArray3.push(x.data))
+console.log(`Post order method: `,testArray3)
+
+let testArray4 = new Array()
+test.inOrderForEach(x => testArray4.push(x.data))
+console.log(`In order method: `,testArray4)
+
+test.insert(200)
+test.insert(300)
+test.insert(400)
+
+test.prettyPrint(test.root)
+console.log(`Current tree is balanced:`,test.isBalanced())
+test.rebalance()
+
+test.prettyPrint(test.root)
+console.log(`Current tree is balanced:`,test.isBalanced())
+
+let testArray5 = new Array()
+test.levelOrderForEach(x => testArray5.push(x.data))
+console.log(`Level order method: `,testArray5)
+
+let testArray6 = new Array()
+test.preOrderForEach(x => testArray6.push(x.data))
+console.log(`Pre-order method: `,testArray6)
+
+let testArray7 = new Array()
+test.postOrderForEach(x => testArray7.push(x.data))
+console.log(`Post order method: `,testArray7)
+
+let testArray8 = new Array()
+test.inOrderForEach(x => testArray8.push(x.data))
+console.log(`In order method: `,testArray8)
+
